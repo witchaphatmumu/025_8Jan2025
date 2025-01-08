15363c4db12e2cc225a8a6361347542adcf91196
witchaphatmumu <witchaphat.n@rmutsvmail.com>
2025-01-08 12:56:29 +0700

    แก้ไขไฟล์ contact.php คร้้งที่1
    ทดลองแก้ไขไฟล์จาก  GitHub Desktop

--- contact.php
+++ contact.php
@@ -17,6 +17,7 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
+                <li><a href="daetil.php">รายละเอียดเพิ่มเติมของกล้วยหอม</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
